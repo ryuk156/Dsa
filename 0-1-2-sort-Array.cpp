9883a489bcaf748530fc1b15617b92675abb1e9a
Yash Patel <49101492+ryuk156@users.noreply.github.com>
2021-05-12 14:22:46 +0530

    Create 0-1-2-sort-Array.cpp

---
+++ 0-1-2-sort-Array.cpp
@@ -0,0 +1,55 @@
+//sort 0,1,2 ARRAY [Dutch flag problem]
+
+
+#include <iostream>
+#include <iomanip>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+	
+	 
+   int a[] = {1,2,0,2,1,2};
+
+  int size =sizeof(a)/sizeof(a[0]);
+   int low =0;
+   int high=size-1;
+   int mid=0;
+   while(mid<=high){
+   	if(a[mid]==0){
+
+   		int temp=a[low];
+      	a[low]=a[mid];
+      	a[mid]=temp;
+      	low++;
+      	mid++;
+   		
+   	}else if(a[mid]==1){
+   		mid++;
+
+   	}else {
+   		int temp=a[mid];
+      	a[mid]=a[high];
+      	a[high]=temp;
+      	high--;
+   	}
+
+   }
+
+
+
+   for (int i = 0; i < size-1; ++i)
+   {
+   cout<<a[i]<<endl;
+   }
+
+
+
+	
+return 0;
+
+
+    }
